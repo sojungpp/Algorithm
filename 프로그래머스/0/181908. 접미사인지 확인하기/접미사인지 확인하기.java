9917f16116b2c509826577c2299ef72187272717
sojungpp <90022940+sojungpp@users.noreply.github.com>
2024-02-24 12:24:11 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.03 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String my_string, String is_suffix) {
+        
+        if(my_string.length() < is_suffix.length()) return 0;
+        else if(my_string.substring(my_string.length()-is_suffix.length()).equals(is_suffix)) return 1;
+        else return 0;
+
+    }
+}
