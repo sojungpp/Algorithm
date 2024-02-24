eb0ceaad596a799d9158a0361c3ac6f8a91a9a3e
sojungpp <90022940+sojungpp@users.noreply.github.com>
2024-02-24 12:19:08 +0900

    [level 0] Title: 배열 만들기 1, Time: 3.75 ms, Memory: 70.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int k) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        
+        for(int i=1; i<=n; i++){
+            if(i%k == 0) answer.add(i);
+        }
+        
+        return answer.stream().mapToInt(i->i).toArray();
+    }
+}
