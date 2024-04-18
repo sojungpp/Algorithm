cb71da1ac6cb150b91bd22db71e01d0e0ef0ab2f
sojungpp <90022940+sojungpp@users.noreply.github.com>
2024-04-19 01:57:11 +0900

    [level 2] Title: 구명보트, Time: 8.95 ms, Memory: 53.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        int left = 0;
+        int right = people.length-1;
+        int answer = 0;
+
+        while(left <= right){
+            answer++;
+            // 같이 탈 수 있는 경우
+            if(people[left]+people[right--]<=limit){
+                left++;
+            }
+        }
+        return answer;
+    }
+}
