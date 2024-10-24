de8bf796fbd3dcc596159a4929de5260c5418b82
sojungpp <90022940+sojungpp@users.noreply.github.com>
2024-10-25 02:57:51 +0900

    [level 3] Title: 여행경로, Time: 0.56 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,42 +1,61 @@
 import java.util.*;
 
 class Solution {
-    String[][] tickets;
-    boolean[] visited;
-    String[] answer = {};
-    List<String> result = new ArrayList<>();
-    int num;
     
-    void dfs(String now, String way, int cnt) {
-        // 종료 조건
-        if (cnt == num) {
-            result.add(way);
-            return;
-        }
-        
-        // 수행 행동
-        // 출발이 2개면 알파벳 순으로 먼저
-        for (int i = 0; i < num; i++) {
-            if (tickets[i][0].equals(now) && !visited[i]) {
-                visited[i] = true;
-                dfs(tickets[i][1], way + "," + tickets[i][1], cnt+1);
-                visited[i] = false;
-            }
-        }
-        return;
-        
-    }
+    boolean[] visited;
+    String[][] tickets;
     
     public String[] solution(String[][] tickets) {
         this.tickets = tickets;
-        this.num = tickets.length;
-        visited = new boolean[num];
-       
-        // a -> b
         
-        dfs("ICN", "ICN", 0);
-        Collections.sort(result);
-        answer = result.get(0).split(",");
-        return answer;
+        Arrays.sort(this.tickets, (a, b) -> a[0].equals(b[0]) ? a[1].compareTo(b[1]) : a[0].compareTo(b[0]));
+        
+        List<String> answer = new ArrayList<>();
+        for(int i=0; i<tickets.length; i++) {
+            if(tickets[i][0].equals("ICN")) {
+                visited = new boolean[tickets.length];
+                visited[i] = true;
+                List<String> list = new ArrayList<>();
+                list.add(tickets[i][0]);
+                list = dfs(i, list);
+
+                
+                if(list.size() == tickets.length+1) {
+                    if(answer.isEmpty() || isFirst(list, answer) < 0) answer = list;
+                }
+            }
+        }
+        return answer.toArray(new String[0]);
+    }
+    
+    private int isFirst(List<String> list, List<String> answer) {
+        for(int i=0; i<list.size(); i++) {
+            int temp = list.get(i).compareTo(answer.get(i));
+            if(temp != 0) return temp;
+        }
+        return 0;
+    }
+    
+    private List<String> dfs(int index, List<String> list) {
+        list.add(tickets[index][1]);
+        if(isFinished()) return list;
+
+        for(int i=0; i<tickets.length; i++) {
+            if(!visited[i] && tickets[i][0].equals(tickets[index][1])) {
+                visited[i] = true;
+                List<String> result = dfs(i, list);
+                if(isFinished()) return result;
+                visited[i] = false;
+                list.remove(list.size()-1);
+            }
+        }
+        return list;
+    }
+    
+    private boolean isFinished() {
+        for(int i=0; i<visited.length; i++) {
+            if(!visited[i]) return false;
+        }
+        return true;
     }
 }
