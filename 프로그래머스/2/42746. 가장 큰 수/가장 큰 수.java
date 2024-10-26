80df37f2cba9cec515fd8dc37b48bd6cfd57d813
sojungpp <90022940+sojungpp@users.noreply.github.com>
2024-10-27 00:44:02 +0900

    [level 2] Title: 가장 큰 수, Time: 2989.31 ms, Memory: 413 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,23 +2,30 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        PriorityQueue<String> pq = new PriorityQueue<>(new Comparator<String>() {
-    	@Override
-		public int compare(String a, String b) {
-    		return (a+b).compareTo(b+a);
-        	}
-    	});
         
-        for(int n : numbers){
-            pq.add(String.valueOf(n));
+        List<Integer> list = new ArrayList<>();
+        for(int n : numbers) {
+            list.add(n);
         }
+        
+        list.sort((a, b) -> {
+            String tempA = String.valueOf(a);
+            String tempB = String.valueOf(b);
+            
+            for(int i=0; i<Math.min(tempA.length(), tempB.length()); i++) {
+                if(tempA.charAt(i) != tempB.charAt(i)) return tempB.charAt(i) - tempA.charAt(i);
+            }
+            
+            return Integer.parseInt(tempB+tempA) - Integer.parseInt(tempA+tempB);
+            
+        });
         
         String answer = "";
-        while(!pq.isEmpty()){
-            answer = pq.poll() + answer;
+        for(int i=0; i<list.size(); i++) {
+            answer += String.valueOf(list.get(i));
         }
-        
-        if(answer.replace("0","").length() == 0) return "0";
+
+        if (answer.charAt(0) == '0') return "0";
         return answer;
     }
 }
