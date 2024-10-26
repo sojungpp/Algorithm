c9d256a8b7363330111930f9260780cda33445b4
sojungpp <90022940+sojungpp@users.noreply.github.com>
2024-10-26 18:31:18 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.91 ms, Memory: 53.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,54 +1,43 @@
 import java.util.*;
 
 class Solution {
-    public static int[] dx = {0, 1, 0, -1};
-    public static int[] dy = {1, 0, -1, 0};
-    public static int answer = -1;
-    public static int n, m;
-    public static int[][] maps;
-    
-    private void dfs(int x, int y, boolean[][] visited){
-        visited[x][y] = true;
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{x, y, 1});
-        visited[0][0] = true;
-        
-        while(!q.isEmpty()){
-            int temp[] = q.poll();
-            x = temp[0];
-            y = temp[1];
-            int cnt = temp[2];
-        
-            if(x == n-1 && y == m-1){
-                answer = cnt;
-                break;
-            }
-        
-            for(int i=0; i<4; i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                if(isValid(nx, ny) && !visited[nx][ny] && maps[nx][ny] == 1) {
-                    visited[nx][ny] = true;
-                    q.add(new int[]{nx, ny, cnt+1});
-                }
-            }
-        }
-    }
-    
-    private boolean isValid(int x, int y){
-        if(x < 0 || y < 0 || x >= n || y >= m) return false;
-        return true;
-    }
+    int[][] maps;
+    boolean[][] visited;
+    int n, m;
+    int answer = Integer.MAX_VALUE;
+    int[] dx = {0, 1, 0, -1};
+    int[] dy = {1, 0, -1, 0};
     
     public int solution(int[][] maps) {
         this.maps = maps;
-        this.n = maps.length; 
-        this.m = maps[0].length; 
-        boolean[][] visited = new boolean[n][m];
+        this.n = maps.length;
+        this.m = maps[0].length;
+        this.visited = new boolean[n][m];
         
-        dfs(0, 0, visited);
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0, 0, 0});
+        
+        while(!q.isEmpty()) {
+            int[] temp = q.poll();
+            
+            if(temp[0] == n-1 && temp[1] == m-1) {
+                return temp[2] + 1;
+            }
+            
+            for(int i=0; i<4; i++) {
+                int tempX = temp[0] + dx[i];
+                int tempY = temp[1] + dy[i];
 
-        return answer;
+                if(isValid(tempX, tempY) && !visited[tempX][tempY] && maps[tempX][tempY] == 1) {
+                    visited[tempX][tempY] = true;
+                    q.add(new int[]{tempX, tempY, temp[2]+1});
+                }
+            }
+        }
+        return -1;
+    }
+    
+    private boolean isValid(int x, int y) {
+        return x>=0 && y>=0 && x<n && y<m;
     }
 }
