92c344599bbd6d36516a3ce243e430d5829292ce
sojungpp <90022940+sojungpp@users.noreply.github.com>
2024-11-01 03:13:33 +0900

    [level 3] Title: N으로 표현, Time: 23.72 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,37 +1,37 @@
 import java.util.*;
 
 class Solution {
-    int N;
-    int number;
-    int answer = 9;
-    
-    void dfs(int size, int now) {
-        if (size>8) return;
-        if (number == now){
-            answer = Math.min(size, answer);
-            return;
-        }
-        
-        int temp = 0;
-        
-        for (int i=0; i<8; i++) {
-            if (size+i<8) {
-                temp = temp*10+N;
-                dfs(size+i+1, now+temp);
-                dfs(size+i+1, now-temp);
-                dfs(size+i+1, now*temp);
-                dfs(size+i+1, now/temp);
-            }
-        }
-    }
-    
     public int solution(int N, int number) {
-        this.N = N;
-        this.number = number;
+        List<Set<Integer>> list = new ArrayList<>();
         
-        dfs(0, 0);
-        if (answer == 9) return -1;
-        return answer;
+        if(N == number) return 1;
         
+        for(int i=0; i<=8; i++) {
+            list.add(new HashSet<>());
+        }
+        
+        list.get(1).add(N);
+        
+        for(int i=2; i<=8; i++) {
+            Set<Integer> nowSet = list.get(i);
+            String num = String.valueOf(N).repeat(i);
+            nowSet.add(Integer.parseInt(num));
+            
+            for(int j=1; j<i; j++) {
+                for(int temp1 : list.get(j)) {
+                    for(int temp2 : list.get(i-j)) {
+                        nowSet.add(temp1 + temp2);
+                        nowSet.add(temp1 - temp2);
+                        nowSet.add(temp1 * temp2);
+                        if(temp2 != 0) nowSet.add(temp1 / temp2);
+                    }
+                }
+            }
+            
+            if(list.get(i).contains(number)) return i;
+
+        }
+        
+        return -1;
     }
 }
