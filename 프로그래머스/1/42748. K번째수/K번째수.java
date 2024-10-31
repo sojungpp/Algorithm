49a8d0b4b1a9532c288792ac073b418a09db361d
sojungpp <90022940+sojungpp@users.noreply.github.com>
2024-10-31 23:34:04 +0900

    [level 1] Title: K번째수, Time: 0.38 ms, Memory: 68.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -4,14 +4,23 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         
-        for(int i=0; i<commands.length; i++){
-            List<Integer> temp = new ArrayList<>();
-            for(int j=commands[i][0]; j<=commands[i][1]; j++){
-                temp.add(array[j-1]);
+        for(int i=0; i<commands.length; i++) {
+            List<Integer> list = new ArrayList<>();
+            for(int j=commands[i][0]-1; j<=commands[i][1]-1; j++) {
+                list.add(array[j]);
             }
-            Collections.sort(temp);
-            answer[i] = temp.get(commands[i][2]-1);
+            Collections.sort(list);
+            
+            int index = 0;
+            for(Integer value : list) {
+                if(index == commands[i][2]-1) {
+                    answer[i] = value;
+                    break;
+                }
+                index++;
+            }
         }
+
         return answer;
     }
 }
