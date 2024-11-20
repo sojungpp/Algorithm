31952085f3551ea03a1be812b2d1e79220b1be4a
sojungpp <90022940+sojungpp@users.noreply.github.com>
2024-11-21 03:13:16 +0900

    [level 2] Title: n^2 배열 자르기, Time: 5.03 ms, Memory: 90.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, long left, long right) {
+
+        
+        int standardX = (int) (left % (long) n);
+        int standardY = (int) (left / (long) n);
+        
+        long standard = right - left;
+        int[] answer = new int[(int)standard+1];
+        int num = 0;
+        
+        while(num != standard+1) {
+            if(standardY <= standardX) answer[num] = standardX+1;
+            else answer[num] = standardY+1;
+            
+            standardY = (standardX+1 >= n) ? standardY+1 : standardY;
+            standardX = (standardX+1)%n;
+            num++;
+        }
+
+        
+        return answer;
+    }
+}
