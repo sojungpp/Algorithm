5c500f429de886885e33bf9ebc3f4d11fa5c99f2
sojungpp <90022940+sojungpp@users.noreply.github.com>
2024-12-28 17:22:26 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,33 +1,32 @@
 import java.util.*;
 
+// 번호, 차례
 class Solution {
     public int[] solution(int n, String[] words) {
-        int[] answer = new int[]{0, 0};
-        int round = 0;
+        
         Set<String> set = new HashSet<>();
-        String standard = "";
-
-        for(int i=0; i<words.length; i++){
-            if(i%n == 0) answer[1]++;
-            if(i==0){
-                standard = words[i];
-                set.add(words[i]);
-                continue;
+        set.add(words[0]);
+        
+        for(int i=1; i<words.length; i++) {
+            String word = words[i];
+            // 한 글자 유무
+            if(word.length() == 1) {
+                return new int[]{i%n+1, i/n+1};
             }
-            if(set.contains(words[i]) || standard.charAt(standard.length()-1) != words[i].charAt(0)){
-                System.out.println(i);
-                // 순서의 사람 = i%n + 1
-                answer[0] = i%n + 1;
-                break;
-            }
-            set.add(words[i]);
-            standard = words[i];
             
-            if(i == words.length-1) return new int[]{0, 0};
-        }
-        
-        return answer;
-        
-        
+            // 옳은 끝말잇기 단어 유무            
+            if(words[i-1].charAt(words[i-1].length()-1) != word.charAt(0)) {
+                return new int[]{i%n+1, i/n+1};
+            }
+            
+            // 등장했던 단어 유무
+            if(set.contains(word)) {
+                return new int[]{i%n+1, i/n+1};
+            }
+            
+            set.add(word);
+            
+        }    
+        return new int[]{0, 0};
     }
 }
