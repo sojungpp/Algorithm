7fb46e8090bbe05248940f74b0f6a8719a4959ed
sojungpp <90022940+sojungpp@users.noreply.github.com>
2025-01-03 11:13:42 +0900

    [level 2] Title: 피로도, Time: 3.05 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,23 +1,40 @@
+import java.util.*;
+
 class Solution {
-    static boolean[] visited;
-    static int answer = 0;
-    
-    private void dfs(int exp, int[][] dungeons, int cnt){
-        for(int i=0; i<dungeons.length; i++){
-            if(!visited[i] && exp >= dungeons[i][0]){
-                visited[i] = true;
-                dfs(exp - dungeons[i][1], dungeons, cnt+1);
-                visited[i] = false;
-            }
-        }
-        answer = Math.max(cnt, answer);
-    }
+    // 최소 필요 피로도, 소모 피로도
+    boolean[] visited;
+    int answer;
+    int[][] dungeons;
     
     public int solution(int k, int[][] dungeons) {
-        // 최소 필요 피로도 (시작 전 필요한 피로도)
-        // 소모 피로도 (탐험 후 소모되는 피로도)
-        visited = new boolean[dungeons.length];
-        dfs(k, dungeons, 0); 
+        this.dungeons = dungeons;
+        this.visited = new boolean[dungeons.length];
+        int now = k;
+        
+        for(int i=0; i<dungeons.length; i++) {
+            now = k;
+            visited = new boolean[dungeons.length];
+            if(now >= dungeons[i][0]) {
+                visited[i] = true;
+                now -= dungeons[i][1];
+                dfs(now, 1);
+            }
+        }
         return answer;
+    }
+    
+    private void dfs(int now, int cnt) {
+        
+        answer = Math.max(cnt, answer);
+        
+        for(int i=0; i<dungeons.length; i++) {
+            if(!visited[i] && now >= dungeons[i][0]) {
+                visited[i] = true;
+                now -= dungeons[i][1];
+                dfs(now, cnt+1);
+                visited[i] = false;
+                now += dungeons[i][1];
+            }
+        }
     }
 }
