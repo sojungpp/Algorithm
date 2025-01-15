baec36511cbffa774d8959d4a013ebf10c26a309
sojungpp <90022940+sojungpp@users.noreply.github.com>
2025-01-15 23:27:07 +0900

    [Lv.3] 징검다리.java

---
+++ 소프티어/LV.3/징검다리.java
@@ -0,0 +1,33 @@
+// https://softeer.ai/practice/6293
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] nums = new int[N];
+        for(int i=0; i<N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[N];
+        for(int i=0; i<N; i++) {
+            dp[i] = 1;
+        }
+        int answer = 0;
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<=i; j++) {
+                if(nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                }
+            }
+        }
+
+        Arrays.sort(dp);
+        System.out.println(dp[N-1]);
+    }
+}
