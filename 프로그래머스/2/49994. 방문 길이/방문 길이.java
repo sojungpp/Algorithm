2c7f8aea6a283abab6d871b43075b6771f89c473
sojungpp <90022940+sojungpp@users.noreply.github.com>
2025-02-19 01:23:08 +0900

    [level 2] Title: 방문 길이, Time: 0.14 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,47 +1,57 @@
 import java.util.*;
 
 class Solution {
-    
-    static boolean[][][] maps = new boolean[11][11][4];
-    
-    private int[] move(String direction, int[] now){
-        switch(direction){
-            case "U": if(isValid(now[0], now[1]+1)) {now[1]+=1;
-                if(!maps[now[0]][now[1]][0] && !maps[now[0]][now[1]-1][1]) maps[now[0]][now[1]][0] = true;
-                                                    }
-                break;
-            case "D": if(isValid(now[0], now[1]-1)) {now[1]-=1;
-                if(!maps[now[0]][now[1]][1] && !maps[now[0]][now[1]+1][0]) maps[now[0]][now[1]][1] = true;}
-                break;
-            case "L": if(isValid(now[0]+1, now[1])) {now[0]+=1; 
-                if(!maps[now[0]][now[1]][2] && !maps[now[0]-1][now[1]][3]) maps[now[0]][now[1]][2] = true;}
-                break;
-            default: if(isValid(now[0]-1, now[1])) {now[0]-=1; 
-                if(!maps[now[0]][now[1]][3] && !maps[now[0]+1][now[1]][2]) maps[now[0]][now[1]][3] = true;}
-                break;
-        }
-        return now;
-    }
-    
-    private boolean isValid(int x, int y){
-        return (x <= 10 && y <= 10 && x >= 0 && y >= 0);
-    }
-    
     public int solution(String dirs) {
-        String[] array = dirs.split("");
-        int[] now = {5, 5};
-        for(String direction : array){
-            now = move(direction, now);
-        }
         
+        char[] inputs = dirs.toCharArray();
+        boolean[][][] visited = new boolean[11][11][5];
         int answer = 0;
-        for(int i=0; i<11; i++){
-            for(int j=0; j<11; j++){
-                for(int z=0; z<4; z++)
-                if(maps[i][j][z]) answer++;
+        int x = 5;
+        int y = 5;
+        for(char input : inputs) {
+            switch(input) {
+                case 'U':
+                    if(isValid(x, y+1)) {
+                        if(!visited[x][y][1]) {
+                            visited[x][y][1] = true;
+                            visited[x][y+1][2] = true;
+                            answer++;
+                        } y += 1;
+                    }
+                    break;
+                case 'D':
+                    if(isValid(x, y-1)) {
+                        if(!visited[x][y][2]) {
+                            visited[x][y][2] = true;
+                            visited[x][y-1][1] = true;
+                            answer++;
+                        } y -= 1;
+                    }
+                    break;
+                case 'R':
+                    if(isValid(x+1, y)) {
+                        if(!visited[x][y][4]) {
+                            visited[x][y][4] = true;
+                            visited[x+1][y][3] = true;
+                            answer++;
+                        } x += 1;
+                    }
+                    break;
+                case 'L':
+                    if(isValid(x-1, y)) {
+                        if(!visited[x][y][3]) {
+                            visited[x][y][3] = true;
+                            visited[x-1][y][4] = true;
+                            answer++;
+                        } x -= 1;
+                    }
+                    break;
             }
         }
-        
         return answer;
+    }
+    
+    private boolean isValid(int x, int y) {
+        return x>=0 && y>=0 && x<=10 && y<=10;
     }
 }
