c86258f5c7e4f1cccfa4c4e8e4612a7b623fa4e8
sojungpp <90022940+sojungpp@users.noreply.github.com>
2025-02-20 02:52:07 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -3,5 +3,5 @@ FROM PLACES
 WHERE HOST_ID IN (SELECT HOST_ID
                  FROM PLACES
                  GROUP BY HOST_ID
-                 HAVING COUNT(HOST_ID) >= 2)
+                 HAVING COUNT(*) >= 2)
 ORDER BY ID;
