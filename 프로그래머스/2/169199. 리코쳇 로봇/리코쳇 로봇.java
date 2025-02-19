40a439e29269c8bb9777c46ad749209ba1e425d9
sojungpp <90022940+sojungpp@users.noreply.github.com>
2025-02-20 02:49:21 +0900

    [level 2] Title: 리코쳇 로봇, Time: 4.05 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -1,73 +1,79 @@
 import java.util.*;
 
 class Solution {
-    static int n;
-    static int m;
-    static int[] end;
-    static boolean[][] visited;
-    static String[][] matrix;
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
+    int[] dx = {0, 1, 0, -1};
+    int[] dy = {1, 0, -1, 0};
+    int n, m;
     
-    private int bfs(int x, int y){
-        visited[x][y] = true;
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{x, y, 0});
+    public int solution(String[] board) {
         
-        while(!q.isEmpty()){
-            int[] now = q.poll();
-            int tempX = now[0];
-            int tempY = now[1];
-            int cnt = now[2];
-            
-            if(tempX==end[0] && tempY==end[1]) return cnt;
-            
-            for(int i=0; i<4; i++){
-                int nowX = tempX;
-                int nowY = tempY;
-                
-                while(isValid(nowX, nowY) && !matrix[nowX][nowY].equals("D")){
-                    nowX += dx[i];
-                    nowY += dy[i];
+        n = board.length;
+        m = board[0].length(); 
+        char[][] maps = new char[n][m];
+        boolean[][] visited = new boolean[n][m];
+        int[] start = new int[2];
+        int[] goal = new int[2];
+        
+        for(int i=0; i<board.length; i++) {
+            char[] inputs = board[i].toCharArray();
+            for(int j=0; j<inputs.length; j++) {
+                maps[i][j] = inputs[j];
+                if(inputs[j] == 'R') {
+                    start[0] = i;
+                    start[1] = j;
+                } else if(inputs[j] == 'G') {
+                    goal[0] = i;
+                    goal[1] = j;
                 }
-                nowX -= dx[i];
-                nowY -= dy[i];
-                
-                if(visited[nowX][nowY]) continue;
-                q.add(new int[]{nowX, nowY, cnt+1});
-                visited[nowX][nowY] = true;
             }
         }
-        return -1;
+        
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(start[0], start[1], 0));
+        visited[start[0]][start[1]] = true;
+        
+        int answer = -1;
+        while(!q.isEmpty()) {
+            Node temp = q.poll();
+
+            if(temp.x == goal[0] && temp.y == goal[1]) {
+                return temp.num;
+            }
+            
+            for(int i=0; i<4; i++) {
+                int tempX = temp.x;
+                int tempY = temp.y;
+                while(true) {
+                    tempX += dx[i];
+                    tempY += dy[i];
+                    if(!isValid(tempX, tempY) || maps[tempX][tempY] == 'D') {
+                        tempX -= dx[i];
+                        tempY -= dy[i];
+                        break;
+                    }
+                }
+                if(!visited[tempX][tempY]) {
+                    visited[tempX][tempY] = true;
+                    q.add(new Node(tempX, tempY, temp.num+1));
+                }
+            }
+        }
+        return answer;
     }
     
-    private boolean isValid(int x, int y){
+    private boolean isValid(int x, int y) {
         return x>=0 && y>=0 && x<n && y<m;
     }
     
-    public int solution(String[] board) {
-        n = board.length;
-        m = board[0].split("").length;
-        int[] start = new int[2];
-        end = new int[2];
-        matrix = new String[n][m];
-        visited = new boolean[n][m];
-
-        for(int i=0; i<n; i++){
-            String[] str = board[i].split("");
-            for(int j=0; j<m; j++){
-                matrix[i][j] = str[j];
-                if(str[j].equals("R")){
-                    start[0] = i;
-                    start[1] = j;
-                }
-                if(str[j].equals("G")){
-                    end[0] = i;
-                    end[1] = j;
-                }
-            }
+    public class Node {
+        int x;
+        int y;
+        int num;
+        
+        public Node(int x, int y, int num) {
+            this.x = x;
+            this.y = y;
+            this.num = num;
         }
-        int answer = bfs(start[0], start[1]);
-        return answer;
     }
 }
