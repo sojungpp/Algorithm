d8ac08e3aa1e0ddbfcdb5e7222d83775822de3c0
sojungpp <90022940+sojungpp@users.noreply.github.com>
2025-02-28 01:00:19 +0900

    [Silver I] Title: 미로 탐색, Time: 120 ms, Memory: 14684 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,53 +1,64 @@
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
 class Main {
+    static int N, M;
     
-    static int N;
-    static int M;
-    static int[][] ways;
-    static boolean[][] visited;
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-    private static void bfs(int x, int y){
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{x, y});
+        char[][] map = new char[N][M];
+        boolean[][] visited = new boolean[N][M];
+        for(int i=0; i<N; i++) {
+            String input = br.readLine();
+            for(int j=0; j<M; j++) {
+                map[i][j] = input.charAt(j);
+            }
+        }
+
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(0, 0, 1));
+        visited[0][0] = true;
         
-        while(!q.isEmpty()){
-            int[] target = q.poll();
-            int targetX = target[0];
-            int targetY = target[1];
+        while(!q.isEmpty()) {
+            Node temp = q.poll();
 
-            for(int i=0; i<4; i++){
-                int nextX = targetX + dx[i];
-                int nextY = targetY + dy[i];
+            if(temp.x == N-1 && temp.y == M-1) {
+                System.out.println(temp.cnt);
+                return;
+            }
 
-                if(nextX < 0 || nextY < 0 || nextX >= N || nextY >= M) continue;
-                if(visited[nextX][nextY] || ways[nextX][nextY] == 0) continue;
+            for(int i=0; i<4; i++) {
+                int tempX = temp.x+dx[i];
+                int tempY = temp.y+dy[i];
 
-                q.add(new int[] {nextX, nextY});
-                ways[nextX][nextY] = ways[targetX][targetY] + 1;
-                visited[nextX][nextY] = true;
+                if(isValid(tempX, tempY) && map[tempX][tempY] == '1' && !visited[tempX][tempY]) {
+                    visited[tempX][tempY] = true;
+                    q.add(new Node(tempX, tempY, temp.cnt+1));
+                }
             }
         }
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
-        ways = new int[N][M];
-        visited = new boolean[N][M];  
-        visited[0][0] = true; 
+    private static boolean isValid(int x, int y) {
+        return x>=0 && y>=0 && x<N && y<M;
+    }
 
-        for(int i=0; i<N; i++){
-            String s = sc.next();
-            for(int j=0; j<M; j++){
-                ways[i][j] = s.charAt(j) - '0';
-            }
+    public static class Node {
+        int x;
+        int y;
+        int cnt;
+
+        public Node(int x, int y, int cnt) {
+            this.x = x;
+            this.y = y;
+            this.cnt = cnt;
         }
-
-        bfs(0, 0);
-        System.out.println(ways[N-1][M-1]);
     }
 }
