033e95cb490b66ddf44f191a7377778c55189b92
sojungpp <90022940+sojungpp@users.noreply.github.com>
2025-02-28 01:37:12 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,36 +1,42 @@
-
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
-    static int answer = 0;
-    static int N, M;
-    static int[][] matrix;
-    static boolean[] visited;
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
 
-    private static void dfs(int index) {
-        visited[index] = true;
-        answer += 1;
-
-        for (int i=1; i<=N; i++) {
-            if (matrix[index][i] == 1 && !visited[i]) dfs(i);
-        }
-    }
-
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
-        matrix = new int[N+1][N+1];
-        visited = new boolean[N+1];
-
-        for (int i=0; i<M; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            matrix[a][b] = matrix[b][a] = 1;
+        boolean[][] computers = new boolean[N+1][N+1];
+        boolean[] worms = new boolean[N+1];
+        for(int i=0; i<M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            computers[start][end] = true;
+            computers[end][start] = true;
         }
 
-        dfs(1);
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        worms[1] = true;
+
+        while(!q.isEmpty()) {
+            int temp = q.poll();
+
+            for(int i=1; i<=N; i++) {
+                if(computers[temp][i] && !worms[i]) {
+                    worms[i] = true;
+                    q.add(i);
+                }
+            }
+        }
+
+        int answer = 0;
+        for(int i=1; i<=N; i++) {
+            if(worms[i]) answer++;
+        }
         System.out.println(answer-1);
     }
 }
