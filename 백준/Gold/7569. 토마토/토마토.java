ab57382a86649df20ac761338d4d4d8b2b6b1e67
sojungpp <90022940+sojungpp@users.noreply.github.com>
2025-02-28 14:41:41 +0900

    [Gold V] Title: 토마토, Time: 592 ms, Memory: 122452 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -3,67 +3,86 @@ import java.lang.*;
 import java.io.*;
 
 class Main {
-
-    static int answer, M, N, H;
+    static int M, N, H;
     static int[][][] tomato;
-    static int[] dx = {0, 1, 0, -1, 0, 0};
-    static int[] dy = {1, 0, -1, 0, 0, 0};
-    static int[] dz = {0, 0, 0, 0, 1, -1};
-    
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        M = sc.nextInt();
-        N = sc.nextInt();
-        H = sc.nextInt();
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
+
         tomato = new int[N][M][H];
-        Queue<int[]> q = new LinkedList<>();
-
-        for(int i=0; i<H; i++){
-            for(int j=0; j<N; j++){
-                for(int k=0; k<M; k++){
-                    tomato[j][k][i] = sc.nextInt();
-                    if(tomato[j][k][i] == 1) q.add(new int[]{j, k, i, 0});
-                }
-            }    
-        }
-
-        if(q.isEmpty()){
-            System.out.println(-1);
-            return;
-        }
-        
-        while(!q.isEmpty()) {
-            int[] temp = q.poll();
-            answer = Math.max(temp[3], answer);
-            
-            for(int i=0; i<6; i++){
-                int nowX = temp[0] + dx[i];
-                int nowY = temp[1] + dy[i];
-                int nowZ = temp[2] + dz[i];
-
-                if(isValid(nowX, nowY, nowZ) && tomato[nowX][nowY][nowZ] == 0){
-                    tomato[nowX][nowY][nowZ] = 1;
-                    q.add(new int[]{nowX, nowY, nowZ, temp[3]+1});
+        boolean isFinished = true;
+        Queue<Node> q = new LinkedList<>();
+        for(int i=0; i<H; i++) {
+            for(int j=0; j<N; j++) {
+                st = new StringTokenizer(br.readLine());
+                for(int k=0; k<M; k++) {
+                    tomato[j][k][i] = Integer.parseInt(st.nextToken());
+                    if(tomato[j][k][i] == 1) q.add(new Node(j, k, i));
+                    if(tomato[j][k][i] == 0) isFinished = false;
                 }
             }
         }
 
-        if(check()) System.out.println(answer);
-        else System.out.println(-1);        
+        if(isFinished) {
+            System.out.println(0);
+            return;
+        }
+        int answer = 0;
+        int[] dx = {0, 0, 1, 0, -1, 0};
+        int[] dy = {0, 0, 0, 1, 0, -1};
+        int[] dz = {1, -1, 0, 0, 0, 0};
+        
+        while(!q.isEmpty()) {
+            answer++;
+
+            if(isFinished()) {
+                System.out.println(answer-1);
+                return;
+            }
+            int size = q.size();
+            for(int i=0; i<size; i++) {
+                Node temp = q.poll();
+                for(int j=0; j<6; j++) {
+                    int tempX = temp.x + dx[j];
+                    int tempY = temp.y + dy[j];
+                    int tempZ = temp.z + dz[j];
+                    if(isValid(tempX, tempY, tempZ) && tomato[tempX][tempY][tempZ] == 0) {
+                        tomato[tempX][tempY][tempZ] = 1;
+                        q.add(new Node(tempX, tempY, tempZ));
+                    }
+                }
+            }
+        }
+        System.out.println(-1);
     }
 
-    private static boolean check() {
-        for(int i=0; i<H; i++){
-            for(int j=0; j<N; j++){
-                for(int k=0; k<M; k++){
+    private static boolean isFinished() {
+        for(int i=0; i<H; i++) {
+            for(int j=0; j<N; j++) {
+                for(int k=0; k<M; k++) {
                     if(tomato[j][k][i] == 0) return false;
                 }
-            }    
+            }
         }
         return true;
     }
 
     private static boolean isValid(int x, int y, int z) {
         return x>=0 && y>=0 && z>=0 && x<N && y<M && z<H;
+    }
+
+    public static class Node {
+        int x;
+        int y;
+        int z;
+
+        public Node(int x, int y, int z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
     }
 }
