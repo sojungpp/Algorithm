b26f96f790554e711bc725420267cffdcf2184bb
sojungpp <90022940+sojungpp@users.noreply.github.com>
2025-04-10 00:55:32 +0900

    [Gold IV] Title: 여행 가자, Time: 228 ms, Memory: 19464 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -1,55 +1,53 @@
 import java.util.*;
+import java.lang.*;
 import java.io.*;
 
 class Main {
     static boolean[][] city;
     static int N, M;
-
+    static boolean[] visited;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());
         city = new boolean[N][N];
-
-        for (int i = 0; i < N; i++) {
+        for(int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                if (Integer.parseInt(st.nextToken()) == 1) {
-                    city[i][j] = true;
-                }
+            for(int j=0; j<N; j++) {
+                if(Integer.parseInt(st.nextToken()) == 1) city[i][j] = true;
             }
         }
 
         int[] plan = new int[M];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) {
-            plan[i] = Integer.parseInt(st.nextToken()) - 1;
+        for(int i=0; i<M; i++) {
+            plan[i] = Integer.parseInt(st.nextToken())-1;
+            
         }
-
+        
         boolean isPossible = true;
-        for (int i = 1; i < M; i++) {
-            boolean[] visited = new boolean[N];
-            if (!dfs(plan[i - 1], plan[i], visited)) {
+        for(int i=1; i<M; i++) {
+            visited = new boolean[N];
+            if(find(plan[i-1], plan[i])) continue;
+            else {
                 isPossible = false;
                 break;
             }
         }
-
-        System.out.println(isPossible ? "YES" : "NO");
+        if(isPossible) System.out.println("YES");
+        else System.out.println("NO");
     }
 
-    private static boolean dfs(int start, int end, boolean[] visited) {
-        if (start == end) return true;
+    // dfs
+    private static boolean find(int start, int end) {
+        if(start == end) return true;
         visited[start] = true;
-
-        for (int i = 0; i < N; i++) {
-            if (city[start][i] && !visited[i]) {
-                if (dfs(i, end, visited)) return true;
+        for(int i=0; i<N; i++) {
+            if(city[start][i] && !visited[i]) {
+                if(find(i, end)) return true;
             }
         }
-
         return false;
     }
 }
