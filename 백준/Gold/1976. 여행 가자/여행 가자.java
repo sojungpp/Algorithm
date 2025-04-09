349ab48c4e7275f7bba589398bcf0b9b3e9cbc4e
sojungpp <90022940+sojungpp@users.noreply.github.com>
2025-04-10 01:03:04 +0900

    [Gold IV] Title: 여행 가자, Time: 152 ms, Memory: 17272 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -3,19 +3,24 @@ import java.lang.*;
 import java.io.*;
 
 class Main {
-    static boolean[][] city;
+    static int[] parent;
     static int N, M;
-    static boolean[] visited;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());
-        city = new boolean[N][N];
+        parent = new int[N];
+        
+        for(int i=0; i<N; i++) {
+            parent[i] = i;
+        }
+        
         for(int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
             for(int j=0; j<N; j++) {
-                if(Integer.parseInt(st.nextToken()) == 1) city[i][j] = true;
+                if(Integer.parseInt(st.nextToken()) == 1) union(i, j);
             }
         }
 
@@ -27,27 +32,30 @@ class Main {
         }
         
         boolean isPossible = true;
+        int root = find(plan[0]);
         for(int i=1; i<M; i++) {
-            visited = new boolean[N];
-            if(find(plan[i-1], plan[i])) continue;
-            else {
+            if(find(plan[i]) != root) {
                 isPossible = false;
                 break;
             }
         }
+        
         if(isPossible) System.out.println("YES");
         else System.out.println("NO");
     }
 
-    // dfs
-    private static boolean find(int start, int end) {
-        if(start == end) return true;
-        visited[start] = true;
-        for(int i=0; i<N; i++) {
-            if(city[start][i] && !visited[i]) {
-                if(find(i, end)) return true;
-            }
+    private static int find(int target) {
+        if(parent[target] != target) {
+            parent[target] = find(parent[target]);
         }
-        return false;
+        return parent[target];
+    }
+
+    private static void union(int start, int end) {
+        int parentA = find(start);
+        int parentB = find(end);
+        if(parentA != parentB) {
+            parent[parentB] = parentA;
+        }
     }
 }
