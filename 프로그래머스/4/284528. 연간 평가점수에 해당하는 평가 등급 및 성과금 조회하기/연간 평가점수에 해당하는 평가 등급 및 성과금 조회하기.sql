5b0ac031b56b66dfce7762f9bc6d642d5c9d06fd
sojungpp <90022940+sojungpp@users.noreply.github.com>
2025-04-13 16:08:44 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,15 +1,16 @@
-SELECT E.EMP_NO, EMP_NAME, CASE WHEN(AVG(SCORE) >= 96) THEN 'S'
-                              WHEN(AVG(SCORE) >= 90) THEN 'A'
-                              WHEN(AVG(SCORE) >= 80) THEN 'B'
-                              ELSE 'C' END GRADE,
-                        CASE WHEN(AVG(SCORE) >= 96) THEN SAL*0.2
-                             WHEN(AVG(SCORE) >= 90) THEN SAL*0.15
-                             WHEN(AVG(SCORE) >= 80) THEN SAL*0.1
-                             ELSE SAL*0 END BONUS
-FROM HR_DEPARTMENT D
-JOIN HR_EMPLOYEES E
-ON D.DEPT_ID = E.DEPT_ID
-JOIN HR_GRADE G
-ON G.EMP_NO = E.EMP_NO
+SELECT HE.EMP_NO, EMP_NAME, 
+    CASE WHEN AVG(SCORE) >= 96 THEN 'S'
+        WHEN AVG(SCORE) >= 90 THEN 'A'
+        WHEN AVG(SCORE) >= 80 THEN 'B'
+        ELSE 'C' END GRADE,
+    CASE WHEN AVG(SCORE) >= 96 THEN SAL*0.2
+        WHEN AVG(SCORE) >= 90 THEN SAL*0.15
+        WHEN AVG(SCORE) >= 80 THEN SAL*0.1
+        ELSE 0 END BONUS
+FROM HR_EMPLOYEES HE
+JOIN HR_DEPARTMENT HD
+ON HE.DEPT_ID = HD.DEPT_ID
+JOIN HR_GRADE HG
+ON HE.EMP_NO = HG.EMP_NO
 GROUP BY EMP_NO, YEAR
-ORDER BY 1;
+ORDER BY 1 ASC;
