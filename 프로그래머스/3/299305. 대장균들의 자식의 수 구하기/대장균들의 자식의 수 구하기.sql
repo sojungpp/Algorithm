4ccffe41cd41feac2b5bf72e5ce5a8c03bf10bc5
sojungpp <90022940+sojungpp@users.noreply.github.com>
2025-05-11 02:02:13 +0900

    [level 3] Title: 대장균들의 자식의 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/299305. 대장균들의 자식의 수 구하기/대장균들의 자식의 수 구하기.sql
+++ 프로그래머스/3/299305. 대장균들의 자식의 수 구하기/대장균들의 자식의 수 구하기.sql
@@ -1,6 +1,6 @@
 SELECT ID, IFNULL((SELECT COUNT(*)
-                   FROM ECOLI_DATA
-                   GROUP BY PARENT_ID
-                   HAVING PARENT_ID = ID), 0) CHILD_COUNT
+                    FROM ECOLI_DATA
+                    GROUP BY PARENT_ID
+                    HAVING PARENT_ID = ID), 0) CHILD_COUNT
 FROM ECOLI_DATA
 ORDER BY 1 ASC;
