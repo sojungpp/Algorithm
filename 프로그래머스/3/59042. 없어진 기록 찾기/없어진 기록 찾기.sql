2a95b0576a0783232d05f94e6a5d01204c029100
sojungpp <90022940+sojungpp@users.noreply.github.com>
2025-05-11 00:56:44 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,5 +1,6 @@
+# 입양을 간 기록은 있는데, 보호소에 들어온 기록이 없는 동물
 SELECT ANIMAL_ID, NAME
-FROM ANIMAL_OUTS AO
+FROM ANIMAL_OUTS
 WHERE ANIMAL_ID NOT IN (SELECT ANIMAL_ID
-                       FROM ANIMAL_INS)
+                    FROM ANIMAL_INS)
 ORDER BY 1, 2;
