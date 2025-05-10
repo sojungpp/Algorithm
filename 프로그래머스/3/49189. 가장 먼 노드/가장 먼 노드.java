5785bb2fde22500379c09e24bb480c562dd09597
sojungpp <90022940+sojungpp@users.noreply.github.com>
2025-05-10 22:05:47 +0900

    [level 3] Title: 가장 먼 노드, Time: 1065.03 ms, Memory: 631 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,50 +1,32 @@
 import java.util.*;
 
+// 1번 노드에서 가장 멀리 떨어진 노드의 갯수 (최단경로로 이동했을 때 간선의 개수가 가장 많은 노드)
 class Solution {
     public int solution(int n, int[][] edge) {
-        boolean[] visited = new boolean[n+1];
-        boolean[][] edgeVisited = new boolean[n+1][n+1];
-        boolean[][] edgeWay = new boolean[n+1][n+1];
-        int[] answer = new int[n+1];
-        Arrays.fill(answer, Integer.MAX_VALUE);
-        answer[1] = 0;
-        
+        int answer = 0;
+        boolean[][] graph = new boolean[n+1][n+1];
         for(int[] e : edge) {
-            edgeWay[e[0]][e[1]] = true;
-            edgeWay[e[1]][e[0]] = true;
+            graph[e[0]][e[1]] = true;
+            graph[e[1]][e[0]] = true;
         }
         
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] visited = new boolean[n+1];
         visited[1] = true;
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{1, 0});
-        
-        int max = 0;
+        q.add(1);
         while(!q.isEmpty()) {
-            int[] temp = q.poll();
-            visited[temp[0]] = true;
-            
-            for(int i=1; i<=n; i++) {
-                if(edgeWay[temp[0]][i] && !visited[i]) {
-                    edgeWay[temp[0]][i] = false;
-                    edgeWay[i][temp[0]] = false;
-                    q.add(new int[]{i, temp[1]+1});
+            int size = q.size();
+            for(int i=0; i<size; i++) {
+                int temp = q.poll();
+                for(int j=1; j<=n; j++) {
+                    if(graph[temp][j] && !visited[j]) {
+                        visited[j] = true;
+                        q.add(j);
+                    }   
                 }
             }
-            if(max < temp[1]) {
-                if(answer[temp[0]] < temp[1]) continue;
-                answer[temp[0]] = Math.min(temp[1], answer[temp[0]]);
-                max = temp[1];
-            } else if(max == temp[1]) answer[temp[0]] = Math.min(temp[1], answer[temp[0]]);
+            answer = size;
         }
-        
-        int cnt = 0;
-        for(int i=1; i<=n; i++) {
-            // System.out.println(answer[i]);
-            if(answer[i] == max) {
-                cnt++;
-            }
-        }
-        // System.out.println(max);
-        return cnt;
+        return answer;
     }
 }
