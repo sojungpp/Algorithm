5d5e938eaee9f95b8e280b1a8dcd739a98f88d1d
sojungpp <90022940+sojungpp@users.noreply.github.com>
2025-05-10 22:33:27 +0900

    [level 3] Title: 순위, Time: 17.59 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -2,32 +2,40 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] results) {
-        boolean[][] win = new boolean[n+1][n+1];
-        boolean[][] lose = new boolean[n+1][n+1];
+        int answer = 0;
         
-        for(int[] result : results) {
-            win[result[0]][result[1]] = true;
-            lose[result[1]][result[0]] = true;
+        int[][] result = new int[n+1][n+1];
+        for(int[] r : results) {
+            result[r[0]][r[1]] = 1;
+            result[r[1]][r[0]] = -1;
+        }
+        
+        for(int i=1; i<=n; i++) {
+            result[i][i] = 0;
         }
         
         for(int i=1; i<=n; i++) {
             for(int j=1; j<=n; j++) {
                 for(int k=1; k<=n; k++) {
-                    if(win[j][i] && win[i][k]) win[j][k] = true;
-                    if(lose[j][i] && lose[i][k]) lose[j][k] = true;
+                    if(result[i][k] == 1 && result[k][j] == 1) {
+                        result[i][j] = 1;
+                        result[j][i] = -1;
+                    }
+                    if(result[i][k] == -1 && result[k][j] == -1) {
+                        result[i][j] = -1;
+                        result[j][i] = 1;
+                    }
                 }
             }
         }
         
-        int answer = 0;
         for(int i=1; i<=n; i++) {
             int cnt = 0;
             for(int j=1; j<=n; j++) {
-                if(win[i][j] || lose[i][j]) cnt++;
+                if(result[i][j] != 0) cnt++;
             }
             if(cnt == n-1) answer++;
         }
-        
         return answer;
     }
 }
